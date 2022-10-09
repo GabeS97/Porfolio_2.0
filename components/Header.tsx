d190b652375aeaa36235b52775127e1ba56c00c5
Gabriel Sitorus <sitorusgg@gmail.com>
2022-10-09 20:41:14 +0700

    navbar animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 items-start flex justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         <SocialIcon
           url="https://www.linkedin.com/in/gabriel-sitorus/"
           target="_blank"
@@ -19,9 +35,24 @@ function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{
+          scale: 0,
+          opacity: 0,
+          x: 500,
+        }}
+        animate={{
+          scale: 1,
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -32,7 +63,7 @@ function Header({}: Props) {
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
           Get in contact with me!
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
